89d5df86a6d25b48f27d8d3879e0dd456f6305bb
Chris MacNamara <cpmacnamara@gmail.com>
2023-05-05 09:43:31 -0400

    add swapi call in useeffect

--- src/pages/StarshipList/StarshipList.jsx
+++ src/pages/StarshipList/StarshipList.jsx
@@ -1,4 +1,24 @@
+// npm packages
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+// services
+import { getAllStarships } from "../../services/sw-api";
+
 const StarshipList = () => {
+  const [starshipList, setStarshipList] = useState([])
+
+  useEffect(() => {
+    const fetchStarshipList = async () => {
+      const starshipData = await getAllStarships()
+      setStarshipList(starshipData)
+    }
+    fetchStarshipList()
+    console.log(starshipList);
+  }, [])
+
+  if(!starshipList.length) return <h1>Loading Starships...</h1>
+
   return (  
     <>
     </>
--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -1,7 +1,6 @@
-const baseUrl = 'https://swapi.dev/api/'
+const baseUrl = 'https://swapi.dev/api'
 
 export async function getAllStarships() {
   const res = await fetch(`${baseUrl}/starships`)
-  console.log(res, 'Res object');
   return res.json()
 }
